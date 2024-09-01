a154571240967fd2da69d1ad975b079e27506dae
Karol Denst <kdenst@gmail.com>
2024-09-01 16:27:10 +0200

    updates

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -5,3 +5,5 @@
 vim.opt.spelllang = "en_us"
 vim.opt.spell = true
 vim.opt.foldnestmax = 1
+vim.opt.shell = "pwsh.exe"
+vim.g.python3_host_prog = "C:/Users/kdens/AppData/Local/Programs/Python/Python312/python.exe"
